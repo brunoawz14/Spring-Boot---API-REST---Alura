1e09784ba3f8c53fad0bf37c20243988bb79f526
Bruno Anjos <brunoanjossantos21@gmail.com>
2026-04-12 14:48:49 -0300

    config Security

--- api/src/main/java/med/voll/api/Controller/AutenticacaoController.java
+++ api/src/main/java/med/voll/api/Controller/AutenticacaoController.java
@@ -2,7 +2,9 @@ package med.voll.api.Controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.Usuario.DadosAutenticacao;
-import org.aspectj.weaver.patterns.IToken;
+import med.voll.api.domain.Usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,12 +21,18 @@ public class AutenticacaoController {
     //Authentication Manager do Spring, responsável por disparar o processo de autenticação.
     @Autowired
     private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody  @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authenticaon = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
 
     }
 }
